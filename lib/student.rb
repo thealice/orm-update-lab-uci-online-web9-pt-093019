6ad8ff9a0c5e54027102a58e83cf7f0112e1aa9e
Alice Freda <alicefreda@gmail.com>
2019-12-17 18:04:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,6 +52,9 @@ class Student
 
   def self.new_from_db(attributes)
     new_student = self.new
+    new_student.name = attributes[0]
+    new_student.grade = attributes[1]
+    new_student.save
     
   end
 
