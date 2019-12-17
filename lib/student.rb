06ac243989993c78e8a7096a7c1ae87e2088249e
Alice Freda <alicefreda@gmail.com>
2019-12-17 18:16:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,7 @@ class Student
   end
 
   def self.new_from_db(row)
-        new_student.id = row[0]
+    new_student.id = row[0]
     name = row[1]
     grade = row[2]
     new_student = self.new(name, grade)
